97fdbe009da16ed61d852034b387c3d8744fb56d
Student <example@example.com>
2020-03-05 14:37:25 -0800

    test IsAWord method passes

--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -10,7 +10,7 @@ namespace Scrabble.Models
       {'a', 1},{'e', 1},{'i', 1},{'o', 1},{'u', 1},{'l', 1},{'n', 1},{'r', 1},{'s', 1},{'t', 1},{'d', 2},{'g', 2},{'b', 3},{'c', 3},{'m', 3},{'p', 3},{'f', 4},{'h', 4},{'v', 4},{'w', 4},{'y', 4},{'k', 5},{'j', 8},{'x', 8},{'q', 10},{'z', 10}
     };
 
-    public static string wordList = System.IO.File.ReadAllText(@"/Users/Guest/Desktop/Scrabble.Solution/Scrabble/Models/WordList.txt");
+    public static string[] wordList = System.IO.File.ReadAllLines(@"/Users/Guest/Desktop/Scrabble.Solution/Scrabble/Models/WordList.txt");
 
     public static char[] WordToChar(string userInput)
     {
@@ -29,9 +29,16 @@ namespace Scrabble.Models
       return totalScore;
     }
 
-  public static bool IsAWord(string userInput)
-  {
-    return false;
-  }
+    public static bool IsAWord(string userInput)
+    {
+      foreach (string entry in wordList)
+      {
+        if (entry.Contains(userInput))
+        {
+          return true;
+        }
+      }
+      return false;
+    }
   }
 }
