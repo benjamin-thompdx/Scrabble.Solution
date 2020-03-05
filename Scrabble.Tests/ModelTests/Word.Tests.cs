062f7f208eeec4c693ec39da07dab08269d4ed95
Student <example@example.com>
2020-03-05 10:32:34 -0800

    add failing test to instantiate dictionary

---
+++ Scrabble.Tests/ModelTests/Word.Tests.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Scrabble.Models;
+
+namespace Scrabble.Test
+{
+  [TestClass]
+  public class WordTests
+  {
+    [TestMethod]
+    public void Dictionary_VerifyObjectWithinDictionary_CharInt()
+    {
+      //Arrange
+      Dictionary<char, int> newDictionary = new Dictionary<char, int>();
+      //Act
+      int points = newDictionary['a'];
+      //Assert
+      Assert.AreEqual(1, points);
+    }
+  }
+
+}
